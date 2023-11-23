6e876786b6632884fcb5b2eb86b399d2f9005205
long.quach <long.quach@blueink.com>
2023-11-23 16:27:33 +0700

    feat: add new test

--- local-test.php
+++ local-test.php
@@ -22,24 +22,77 @@ foreach ($lines as $line) {
     }
 }
 ### --- end block --- ###
-$_name = 'SDK NAME TEST';
-$_email = 'example_email@email.com';
-$_file_url = 'https://www.irs.gov/pub/irs-pdf/fw9.pdf';
-$client = new Blueink\Client(getenv("BLUEINK_PRIVATE_API_KEY"));
-$packet = Blueink\Packet::create(null, $_name, ['email' => $_email]);
+function test_bundles() {
+    $_name = 'SDK NAME TEST';
+    $_email = 'example_email@email.com';
+    $_file_url = 'https://www.irs.gov/pub/irs-pdf/fw9.pdf';
+    $client = new Blueink\Client(getenv("BLUEINK_PRIVATE_API_KEY"));
+    $packet = Blueink\Packet::create(null, $_name, ['email' => $_email]);
 
-$document = Blueink\Document::create(null, ['file_url' => $_file_url]);
-$bundle = new Blueink\Bundle(['packets' => [$packet], 'documents' => [$document]]);
+    $document = Blueink\Document::create(null, ['file_url' => $_file_url]);
+    $bundle = new Blueink\Bundle(['packets' => [$packet], 'documents' => [$document]]);
 
-try {
-    $new_bundle = $client->bundles->create($bundle);
-    // $client->bundles->list('2', '2');
-    // $client->bundles->cancel($new_bundle['id']);
-    $client->bundles->retrieve($new_bundle['id']);
-} catch (GuzzleException\RequestException $e) {
-    # handle the exception
-    echo 'Got an exception';
-    $response = $e->getResponse();
-    echo "Status Code: " . $response->getStatusCode() . "\n";
-    echo "Reason: " . $response->getReasonPhrase() . "\n";
+    try {
+        $new_bundle = $client->bundles->create(['body' => $bundle]); 
+        $client->bundles->list('2', '1');
+        $client->bundles->cancel($new_bundle['id']);
+        $client->bundles->retrieve($new_bundle['id']);
+    } catch (GuzzleException\RequestException $e) {
+        # handle the exception
+        echo 'Got an exception';
+        $response = $e->getResponse();
+        echo "Status Code: " . $response->getStatusCode() . "\n";
+        echo "Reason: " . $response->getReasonPhrase() . "\n";
+    }
 }
+
+function test_persons() {
+    $json = '{
+        "name": "Tom Jones",
+        "metadata": {
+            "occupation": "legend"
+        },
+        "channels": [
+            {
+                "email": "tom.jones@example.com",
+                "kind": "em"
+            },
+            {
+                "phone": "505 555 1234",
+                "kind": "mp"
+            }
+        ]
+    }';
+    $json2 = '{
+        "name": "=== Tom Jones",
+        "metadata": {
+            "occupation": "legend"
+        },
+        "channels": [
+            {
+                "email": "tom.jones@example.com",
+                "kind": "em"
+            },
+            {
+                "phone": "505 555 1234",
+                "kind": "mp"
+            }
+        ]
+    }';
+    $client = new Blueink\Client(getenv('BLUEINK_PRIVATE_API_KEY'));
+    try {
+        $person = $client->persons->create(['json' => $json]);
+        $client->persons->list('2','1');
+        $client->persons->update($person['id'], ['json' => $json2]);
+        $client->persons->retrieve($person['id']);
+        $client->persons->delete($person['id']);
+    } catch (GuzzleException\RequestException $e) {
+        # handle the exception
+        echo 'Got an exception';
+        $response = $e->getResponse();
+        echo "Status Code: " . $response->getStatusCode() . "\n";
+        echo "Reason: " . $response->getReasonPhrase() . "\n";
+    }
+}
+// test_bundles();
+test_persons();
